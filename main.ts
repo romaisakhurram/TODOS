dfb73d3f7957f77cc42fb10626fc20a98b14f9c2
Your Name <you@example.com>
2024-04-20 22:41:14 +0500

    create a todo  list

---
+++ main.ts
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+let todos : string[] = ["Rafay" , "Uzair" , "Mahma"]
+
+async function createTodos(todos:string [])
+{
+ do{
+    let answer = await inquirer.prompt
+   ([
+     {
+      name : "select",
+      type : "list",
+      message : "Select an operation",
+      choices : ["Add" , "Update" , "View" , "Delete"]
+     },
+   ]);
+   //console.log(todos);
+   if(answer.select== "Add")
+   {
+   let addTodo = await inquirer.prompt
+   ([
+      {
+        name : "todo",
+        type : "input",
+        message : "Add the items in the list"
+      },
+   ]);
+   todos.push(addTodo.todo);
+   todos.forEach(todo => console.log(todo)); 
+   //console.log(todos); 
+   }
+
+   if(answer.select == "Update")
+   {
+    let updateTodo = await inquirer.prompt
+    ([
+     {
+        name : "todo",
+        type : "list",
+        message : "Update the items in the list",
+        choices : todos.map(item => item)
+     },
+    ]);
+    let addTodo = await inquirer.prompt
+    ([
+     {
+        name : "todo",
+        type : "input",
+        message : "Add the items in the list"
+     },
+    ]);
+    let newTodo = todos.filter(val =>val !== updateTodo.todo); 
+    todos = [...newTodo,addTodo.todo];
+    todos.forEach(todo => console.log(todo)); 
+   };
+
+   if(answer.select == "View")
+   {
+    console.log("*******TODO LIST********");
+    console.log(todos);
+    console.log("************************");
+   };
+
+   if(answer.select == "Delete")
+    {
+     let deleteTodo = await inquirer.prompt
+     ([
+       {
+         name : "todo",
+         type : "list",
+         message : "Delete the items",
+         choices : todos.map(item => item)
+       },
+     ]);
+     let newTodo = todos.filter(val => val !== deleteTodo.todo); 
+     todos = [...newTodo];
+     todos.forEach(todo => console.log(todo)); 
+     console.log(todos)
+    };
+  } while(true);
+}
+createTodos(todos);    
